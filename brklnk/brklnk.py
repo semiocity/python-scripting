39c6e936049c17e33cff41db4fbd887ab5847bd3
Grégory Dziedzic <gregory.dziedzic@gmail.com>
2021-11-12 12:53:29 +0100

    Global variables for the sets of visited URLs and broken URLs turned into local variables passed to the recursive function

--- brklnk/brklnk.py
+++ brklnk/brklnk.py
@@ -8,35 +8,30 @@ from bs4 import BeautifulSoup
 import requests
 import re
 
-from requests.api import head
-
-visited_urls = set()
-broken_links = set()
-
-def brklnk(url, depth, verbose):
+def brklnk(url, depth, verbose, visited_urls, broken_links):
+    # print("Visited URLs: {}\n".format(visited_urls))
     if verbose:
         print("Testing: {}".format(url), end=" ")
-    head = requests.head(url, allow_redirects=True)
+    head = requests.head(url, allow_redirects = True)
     visited_urls.add(url)
     if head.status_code == 200:
         if verbose:
             print("---> OK")
 
         if depth >= 1:
-            page = requests.get(url, allow_redirects=True)
+            page = requests.get(url, allow_redirects = True)
             soup = BeautifulSoup(page.content, 'html.parser')
             for link in soup.findAll("a", href=True):
                 absolute_path = urljoin(url, link['href'])
                 protocol = absolute_path[:absolute_path.index(":")]
                 if protocol in {"http", "https"} and absolute_path not in visited_urls:
-                    brklnk(absolute_path, depth-1, verbose)
+                    brklnk(absolute_path, depth-1, verbose, visited_urls, broken_links)
     else:
         if verbose:
             print("\nThis link is broken!\n")
         else:
             print("Broken link: {}".format(url))
         broken_links.add(url)
-
 
 def main():
     # build an empty parser
@@ -45,7 +40,13 @@ def main():
     parser.add_argument("--depth", "-d", type=str, default = "1", help="Depth of recursive search (default value: 1)")
     parser.add_argument("--verbose", "-v", help="Verbose", action="store_true")
     args = parser.parse_args()
-    brklnk(args.url, int(args.depth), args.verbose)
+    broken_links = set()
+    brklnk(args.url, int(args.depth), args.verbose, set(), broken_links)
+    if args.verbose:
+        print("\n\nList of broken links:")
+        for link in broken_links:
+            print (link)
+
 
 if __name__ == '__main__':
     main ()
