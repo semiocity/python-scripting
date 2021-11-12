31811a7c16b7511b71ca8b611219907f60549ed2
Grégory Dziedzic <gregory.dziedzic@gmail.com>
2021-11-12 01:02:53 +0100

    Fully working script

--- brklnk/brklnk.py
+++ brklnk/brklnk.py
@@ -8,63 +8,33 @@ from bs4 import BeautifulSoup
 import requests
 import re
 
+from requests.api import head
 
-def brklnk(url):
-    print(url)
-    print(urlparse(url))
-    # regexing4links = re.compile(r'href=\".*\"')
-    page = requests.get(url, allow_redirects=True)
-    if page.status_code == 200:
-        soup = BeautifulSoup(page.content, 'html.parser')
-    print (soup)
+visited_urls = set()
 
-    links = []
-    for link in soup.findAll("a", href=True):
-        print("Href: {}".format(link['href']))
-        absolute_path = urljoin(url, link['href'])
-        links.append(absolute_path)
+def brklnk(url, depth):
+    head = requests.head(url, allow_redirects=True)
+    visited_urls.add(url)
+    if head.status_code == 200:
+        if depth >= 1:
+            page = requests.get(url, allow_redirects=True)
+            soup = BeautifulSoup(page.content, 'html.parser')
+            for link in soup.findAll("a", href=True):
+                absolute_path = urljoin(url, link['href'])
+                protocol = absolute_path[:absolute_path.index(":")]
+                if protocol in {"http", "https"} and absolute_path not in visited_urls:
+                    brklnk(absolute_path, depth-1)
+    else:
+        print("Broken link: {}".format(url))
 
-    broken_links = []
-    for link2Btested in links:
-        print ("Testing: {}".format(link2Btested))
-        page = requests.get(link2Btested, allow_redirects=False)
-        if page.status_code != 200:
-            broken_links.append (link2Btested)
-
-    print("Broken links: {}".format(broken_links))
-    # links = re.findall(r'href=\".*\"', str(soup))
-    # for ilink, link in enumerate(links):
-    #     links[ilink] = url+link[6:-1]
-    # print (links)
-    # open("landing_page", "wt").write(page.content)
-    # with open(url) as fp:
-    #     soup = BeautifulSoup(fp, 'html.parser')
-    # for root, _, filenames in os.walk(directoryPath):
-    #     for filename in filenames:
-    #         # print ("Root: {}. Filename: {}".format(root, filename))
-
-    #         # if filename.endswith('.pyc'):
-    #         #     os.remove(os.path.join(root, filename))
-    #         # print ("Début chemin", os.path.join(root, filename), "Fin chemin")
-    #         if filename[0]==".":
-    #             continue
-    #         with open(os.path.join(root, filename)) as file:
-    #             print("Fichier ouvert")
-    #             try:
-    #                 for ligne in file:
-    #                     # print(ligne)
-    #                     entre_guillements = re.findall(r'\"[^\"]*\"', ligne)
-    #                     for phrase in entre_guillements:
-    #                         print(phrase)
-    #             except:
-    #                 continue
 
 def main():
     # build an empty parser
     parser = argparse.ArgumentParser()
-    parser.add_argument("url", type=str, help="URL à examiner")
+    parser.add_argument("url", type=str, help="URL where to look for broken links")
+    parser.add_argument("--depth", "-d", type=str, default = "1", help="Depth of the recursive search")
     args = parser.parse_args()
-    brklnk(args.url)
+    brklnk(args.url, int(args.depth))
 
 if __name__ == '__main__':
     main ()
