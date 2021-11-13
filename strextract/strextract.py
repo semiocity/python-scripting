2fbf53e8bf8db596602d8ebe0f37ad6781c91753
Grégory Dziedzic <gregory.dziedzic@gmail.com>
2021-11-13 21:08:46 +0100

    Fully working script with all required options

--- strextract/strextract.py
+++ strextract/strextract.py
@@ -11,11 +11,11 @@ def file_suffix(filename):
     else:
         return ""
 
-def strextract(directoryPath, suffix, allfiles):
+def strextract(directoryPath, suffix, allfiles, path_required):
     precompiled_regex = re.compile(r'\"[^\"]*\"')
     for root, _, filenames in os.walk(directoryPath):
         for filename in filenames:
-            if (filename[0] == "." and not(allfiles)) or file_suffix(filename) != suffix :
+            if (filename[0] == "." and not(allfiles)) or ((suffix != "") and (file_suffix(filename) != suffix)) :
                 continue
             with open(os.path.join(root, filename)) as file:
                 try:
@@ -23,7 +23,7 @@ def strextract(directoryPath, suffix, allfiles):
                         # print(ligne)
                         between_quotes = precompiled_regex.findall(ligne)
                         for phrase in between_quotes:
-                            print(phrase)
+                            print("{}\t{}".format(os.path.join(root, filename),phrase))
                 except:
                     continue
 
@@ -32,10 +32,11 @@ def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("dir", type=str, help="Directory from which the string extraction has to be performed recursively in all files and subdirectories")
     parser.add_argument("--suffix", "-s", type=str, default = "", help="Suffix of the files to be inspected")
-    parser.add_argument("--all", "-a", default = "", help="Suffix of the files to be inspected")
+    parser.add_argument("--all", "-a", default = False, help = "Also inspect hidden files", action="store_true")
+    parser.add_argument("--path", "-p", default = False, help = "Display full path of the file being inspected", action="store_true")
 
     args = parser.parse_args()
-    strextract(args.dir, args.suffix)
+    strextract(args.dir, args.suffix, args.all, args.path)
 
 if __name__ == '__main__':
     main ()
